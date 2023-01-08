68b86e8a5267fd6714312178c3e050d3176aacf5
Mateussales1 <s.mateussales@gmail.com>
2023-01-08 20:45:56 -0300

    first commit

---
+++ src/main/java/academy/dev/springboot/controller/AnimeController.java
@@ -0,0 +1,19 @@
+package academy.dev.springboot.controller;
+
+
+import academy.dev.springboot.domain.Anime;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("anime")
+public class AnimeController {
+
+    @GetMapping(path = "list")
+    public List <Anime> list(){
+        return List.of(new Anime("DBZ"), new Anime("Attack On Titan"));
+    }
+}
