6bab46862cc8ec8fd2c535b407ca10037060b7fd
Mateus Sales de Albuquerque Correia <mateus.correia@frutuosoadvocacia.adv>
2023-01-20 12:51:05 -0300

    pom.xml fixed

--- src/main/java/academy/dev/springboot/service/AnimeService.java
+++ src/main/java/academy/dev/springboot/service/AnimeService.java
@@ -1,16 +1,25 @@
 package academy.dev.springboot.service;
 
 import academy.dev.springboot.domain.Anime;
-import academy.dev.springboot.repository.AnimeRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
-public class AnimeService implements AnimeRepository {
-    public List<Anime> listAll(){
-        return List.of(new Anime(1L,"DB-GT"), new Anime(2L, "Attack On Titan"));
+public class AnimeService {
+    private List<Anime> animes = List.of(new Anime(1L, "Boku No Hero"), new Anime(2L, "Berserk"));
+
+    // private final AnimeRepository animeRepository;
+    public List<Anime> listAll() {
+        return animes;
     }
 
-
+    public Anime findById(long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
+    }
 }
